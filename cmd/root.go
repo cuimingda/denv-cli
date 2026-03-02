005e28eaa88d6110b1e696db5bc1862cf7f899d3
cuimingda <cuimingda@gmail.com>
2026-03-02 10:46:59 +0800

    初始化 denv CLI 项目 with cobra

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,13 @@ import (
     "github.com/spf13/cobra"
 )
 
+const version = "0.0.1"
+
 func NewRootCmd() *cobra.Command {
     rootCmd := &cobra.Command{
         Use:   "denv",
         Short: "denv command line interface",
+        Version: version,
         Run: func(cmd *cobra.Command, _ []string) {
             _ = cmd.Help()
         },
